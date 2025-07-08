22566a2ad25dbf39d70874ddf43f3bd8cfe7ae57
wanglaoea <marsyasasaputri495@gmail.com>
2025-07-08 08:48:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://frontcakar.vercel.app'],
+    origin: ['https://cakar-iota.vercel.app'],
     methods: ['POST']
 }));
 
